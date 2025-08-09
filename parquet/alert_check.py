044a6fca34adf3f5fc2cf1cb01b17dd7c12abec9
Nanda G <nandasguru@gmail.com>
2025-08-09 03:22:45 +0000

    Basic comparison works now

--- parquet/alert_check.py
+++ parquet/alert_check.py
@@ -6,6 +6,8 @@ import pathlib
 import time
 import pytz
 
+import random # Only for local testing without load simulator
+
 
 # Class used to read lookup table
 class QuantileLookupPandas:
@@ -17,7 +19,7 @@ class QuantileLookupPandas:
             ["daily_window_number", "day_of_week", "power_bin_idx"],
             inplace=True,
         )
-        print("[INFO] Lookup table loaded.")
+        print("[INFO] Lookup table loaded.\n")
 
     def daily_window_number(self, ts: datetime.datetime) -> int:
         return ts.hour * 4 + ts.minute // 15 + 1
@@ -109,17 +111,28 @@ if __name__ == "__main__":
     start = user_to_unix_timestamp(8, 7, 2025, 14, 0, 0)
     end = user_to_unix_timestamp(8, 7, 2025, 16, 0, 0)
 
+    # When collecting real data
+
     step = 60
 
     # This variable keeps track of the current time window
     current_time_window = 15
     time_window_index = 0
 
+    # Define variables used for comparison
+    # Generate alert if power outside of percentile range 5 times in a row or
+    # whatever it's set to [percentile_range_threshold], within the 15 minute window
+    percentile_range_threshold = 5
+    under_05_in_a_row = 0
+    over_95_in_a_row = 0
+
     while True:
 
+        one_or_zero_random = random.choices([1,0], weights=[95,5])[0]
+        # print(one_or_zero_random)
 
         if time_window_index == 0:
-            print("[TEST] --- Time window start ---")
+            print("[TEST] --- Time window start ---\n")
 
         time_window_index += 1
 
@@ -128,8 +141,6 @@ if __name__ == "__main__":
 
 
         now = int(time.time())
-
-        # Change how often data comes in
         
         # Use these for using current time
         # start = round_down_to_minute((now-step))
@@ -143,26 +154,87 @@ if __name__ == "__main__":
             dt = datetime.datetime.fromtimestamp(ts, pytz.utc).astimezone(pst)
             dt_str = dt.strftime("%Y-%m-%d %H:%M:%S %Z")
 
+            # Use a FAKE metric value, in this case S1, to simulate too much power
+            # before testing with load simulator, just for the sake of testing the code
+            # for alerts. Remove this part later when doing the actual thing fr
+            # Remove when actual thingy is working
+            # ====Remove starts here====
+
+            # Calculating fake metric value
+            # Since the 'real' data is already over the 95th percentile I have, use 
+            # a smaller number every once in a while so it's within the percentile range
+            if one_or_zero_random == 0:
+                val = 15 # Use this fake value to show power ok
+
+            # ====Remove ends here====
+
+            print(f"{dt_str}: {metric} = {val}")
+
             try:
-                p05, p95 = lookup.predict(dt, val)
-                print(f"5 pctl is: {p05}, 95 pctl is: {p95}")
+                p05, p95 = lookup.predict(dt, val)     
+                print(f"5 pctl is: {p05:.2f}, 95 pctl is: {p95:.2f}\n")
             except ValueError as e:
                 print(f"Lookup error: {e}")
+                break
+
+            # At this point, have both metric value and 5th/95th percentile values
+            # can do comparison(s) now
+
+            # Currently, only test case for generating alert is if the metric value is
+            # outside the percentile range 5 times in a row within the 15 minute window
+
+            # Check under 05th percentile
+            if val < p05:
+                under_05_in_a_row += 1 # increase under 5 pctl count
+                over_95_in_a_row = 0 # reset the over 95 pctl count
+
+                # Happened enough times to generate alert
+                if under_05_in_a_row == percentile_range_threshold:
+                    print("\n==========THIS IS THE ALERT==========\n")
+                    under_05_in_a_row = 0 # once alert, reset back to 0
+
+            # Check over 95th percentile
+            elif val > p95:
+                over_95_in_a_row += 1 # increase over 95 pctl count
+                under_05_in_a_row = 0 # reset the under 5 pctl count
+
+                # Happened enough time to generate alert
+                if over_95_in_a_row == percentile_range_threshold:
+                    print("\n==========THIS IS THE ALERT==========\n")
+                    over_95_in_a_row = 0 # once alert, reset back to 0
+
+            # Reset alert counts if metric is within percentile range
+            else:
+                over_95_in_a_row = 0
+                under_05_in_a_row = 0
+
+                   
+        
+            # print counts
+            print(f"[TEST] under 5 count:     {under_05_in_a_row}")
+            print(f"[TEST] over 95 count:     {over_95_in_a_row}\n")
 
 
-            print(f"{dt_str}: {val}")
         else:
             print("[WARNING] No data received")
+            break
 
         start += step
         end += step
 
         if time_window_index == current_time_window:
-            print("[TEST] --- Time window end ---")
+            print("[TEST] --- Time window end ---\n")
             time_window_index = 0
 
+            # Since we are only checking within each time window, we don't want to give an
+            # alert if there are 5 outliers in a row if it crosses a time window (for now)
+            # Check if we are going to cross a time window, if yes, reset the counts
 
-            
+            under_05_in_a_row = 0
+            over_95_in_a_row = 0              
+
+
+
 
         time.sleep(2) # this should be step, change it back then remove this comment
-                      # when finished testing
+                      # when finished testing/doing live data
